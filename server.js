c18e675026f8a5985557bed4fc4d4873e833ec17
TonyS <tonysilvasd@gmail.com>
2019-01-08 15:36:56 -0800

    adds initial project files with working contact storage

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const request = require('superagent');
+require('dotenv').config()
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//Here we're setting the view directory to be ./views
+//thereby letting the app know where to find the template files
+app.set('views', './views');
+
+//Defult engine to be ejs
+// note we don't need to require it, express does for us
+app.set('view engine', 'ejs');
+
+//res.render instead of res.send to send output of template by filename
+app.get('/', (req, res) => {
+    const data = {
+        person: {
+            firstName: 'Tony',
+            lastName: 'Silva'
+        }
+    }
+    //Now data is the second argument passed to template render method, data being the local variable above
+    res.render('index', data);
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+const mailchimpInstance = 'us7';
+const listUniqueId = process.env.LIST_UNIQUE_ID;
+const API_KEY = process.env.MAILCHIMP_API_KEY;
+app.post('/thanks', (req, res) => {
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + API_KEY ).toString('base64'))
+        .send({
+          'email_address': req.body.email,
+          'status': 'subscribed',
+          'merge_fields': {
+            'FNAME': req.body.firstName,
+            'LNAME': req.body.lastName
+          }
+        })
+        .end(function(err, response) {
+            if(response.status < 300 || (response.status === 400)) {
+                res.render('thanks', { contact: req.body })
+            } else {
+                console.log(err);
+                res.send('Sign Up Failed :(');
+            }
+        }) 
+});
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
